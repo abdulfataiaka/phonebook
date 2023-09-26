edf426ef04b8f6607b7b8a4208dbc079cc8ffd27
Abdulfatai Aka <abdulfataiaka@gmail.com>
2023-09-26 09:48:01 +0100

    make add page functional

--- src/components/UpsertContactPage/index.js
+++ src/components/UpsertContactPage/index.js
@@ -1,19 +1,57 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; 
+import './styles.css'
 import Header from '../Header';
+import contacts from '../../mocks';
 import PlaceholderImage from '../../images/placeholder.svg';
 
 const UpsertContactPage = () => {
-    const handleSave = () => {
-        console.log('Save button has been clciked');
+    const navigate = useNavigate();
+    const [fullName, setFullName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+
+    const handleSave = (event) => {
+        const contact = {
+            name: fullName,
+            phone: phoneNumber,
+            image: PlaceholderImage,
+        }
+
+        // Save into the contacts list store
+        contacts.push(contact);
+
+        // Redirect to the contact list page
+        navigate('/');
+    }
+
+    const handleFullNameChange = (event) => {
+        const newValue = event.target.value;
+        setFullName(newValue);
+    }
+
+    const handlePhoneNumberChange = (event) => {
+        const newValue = event.target.value;
+        setPhoneNumber(newValue);
     }
 
     return (
         <div>
             <Header />
-            <div>
+            <div className="upsert-form-container">
                 <img src={PlaceholderImage} alt="preview" />
                 <input type="file" />
-                <input type='text' placeholder='Enter your full name' />
-                <input type='text' placeholder='Enter your phone number' />
+                <input
+                    onChange={handleFullNameChange}
+                    value={fullName}
+                    type='text'
+                    placeholder='Enter your full name'
+                />
+                <input
+                    onChange={handlePhoneNumberChange}
+                    value={phoneNumber}
+                    type='text'
+                    placeholder='Enter your phone number'
+                />
                 <button onClick={handleSave}>Save</button>
             </div>
         </div>
